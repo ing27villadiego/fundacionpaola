2affa0c2f11f6deac2e3ef47705de8067d32c66b
@ingjvilladiegot <ingsistemajvilladiegot@gmail.com>
2020-07-16 17:24:29 -0500

    se agrego imagen y contenido para el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+<div class="contenedor-img">
+    <img src="{{ asset('imagenes/building.svg') }}" alt="img-building">
+</div>
+<main>
+<h1>
+    nuestro sitio web se encuentra en construcción
+</h1>
+<div class="contenedor-box">
+    <div class="box">
+        <div class="box-number">
+            <h2>00</h2>
+        </div>
+        <div class="box-time">
+            Dias
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-number">
+            <h2>00</h2>
+        </div>
+        <div class="box-time">
+            Horas
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-number">
+            <h2>00</h2>
+        </div>
+        <div class="box-time">
+            Minutos
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-number">
+            <h2>00</h2>
+        </div>
+        <div class="box-time">
+            Segundos
         </div>
     </div>
 </div>
+</main>
 @endsection
